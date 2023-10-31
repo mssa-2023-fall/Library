f84ea04a18ae0df2e3d6211426a59359754ac066
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-10-31 13:28:28 -0500

    Tuesday Challenge

---
+++ MortgageCalculatorLibrary/GlizzyServices/Interfaces.cs
@@ -0,0 +1,38 @@
+﻿namespace GlizzyServices
+{
+	interface ICurrencyConverter
+	{
+		Money Convert(string from, string to, decimal amountToConvertFrom);
+	}
+
+    public record class Money
+    {
+        string Currency;
+        decimal Amount;
+    }
+
+    interface INotifier
+    {
+        void Notify(string addressTo,string message);
+    }
+
+    interface IGlizzyFacts
+    {
+        GlizzyFact GetGlizzyFact();
+        bool AddGlizzyFacts(GlizzyFact fact );
+    }
+
+    public record class GlizzyFact
+    {
+        int FactId;
+        string Source;
+        string Fact;
+    }
+
+    interface IMortgageCalcualtor
+    {
+        decimal CalculateMonthlyPayment();
+    }
+
+
+}
