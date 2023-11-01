a98b0a32be41309b06203209de1678895334b09e
Chris Rodriguez <christian.rodriguez6497@outlook.com>
2023-11-01 09:46:58 -0400

    SMS notifier

--- MortgageCalculatorLibrary/GlizzyServices/Interfaces.cs
+++ MortgageCalculatorLibrary/GlizzyServices/Interfaces.cs
@@ -5,34 +5,34 @@
 		Money Convert(string from, string to, decimal amountToConvertFrom);
 	}
 
-    public record class Money
-    {
-        string Currency;
-        decimal Amount;
-    }
+	public record class Money
+	{
+		string Currency;
+		decimal Amount;
+	}
 
     interface INotifier
     {
-        void Notify(string addressTo,string message);
+		Task Notify(string addressFrom, string addressTo, string message);
     }
 
     interface IGlizzyFacts
-    {
-        GlizzyFact GetGlizzyFact();
-        bool AddGlizzyFacts(GlizzyFact fact );
-    }
+	{
+		GlizzyFact GetGlizzyFact();
+		bool AddGlizzyFacts(GlizzyFact fact );
+	}
 
-    public record class GlizzyFact
-    {
-        int FactId;
-        string Source;
-        string Fact;
-    }
+	public record class GlizzyFact
+	{
+		int FactId;
+		string Source;
+		string Fact;
+	}
 
-    interface IMortgageCalcualtor
-    {
-        decimal CalculateMonthlyPayment();
-    }
+	interface IMortgageCalcualtor
+	{
+		decimal CalculateMonthlyPayment();
+	}
 
 
 }
---
+++ MortgageCalculatorLibrary/GlizzyServices/SmsNotifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using GlizzyServices;
+
+public class SmsNotifier : INotifier
+{
+    private const string AccountSid = "ACd6043574f55bb1b7a2814331f826250a";
+    private const string AuthToken = "82c009bd1051e88ea0899308105c9a8d"; //blah blah, environment variable, blah.
+    private const string FromPhoneNumber = "+18557015642";
+
+    static SmsNotifier()
+    {
+        TwilioClient.Init(AccountSid, AuthToken);
+    }
+
+    public async Task Notify(string addressFrom, string addressTo, string message)
+    {
+        var messageOptions = new CreateMessageOptions(new PhoneNumber(addressTo));
+        messageOptions.From = new PhoneNumber(FromPhoneNumber);
+        messageOptions.Body = message;
+
+        var messageResource = await MessageResource.CreateAsync(messageOptions);
+
+        Console.WriteLine(messageResource.Body);
+    }
+}
